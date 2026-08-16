a82a29725fa165a453396281370360cd3ac0f522
NooraPanahi <nourap064@gmail.com>
2026-08-16 14:45:56 +0330

    fix: change prefix match logic in levenshtein algorithm

--- src/algorithms/levenshtein_search.cpp
+++ src/algorithms/levenshtein_search.cpp
@@ -1,9 +1,7 @@
 #include "levenshtein_search.h"
 
 levenshtein_search::levenshtein_search(interface_graph &graph):graph(graph)
-{
-   
-}
+{}
 
 std::string levenshtein_search::to_lower(const std::string &text){
     std::string result = text;
@@ -12,71 +10,75 @@ std::string levenshtein_search::to_lower(const std::string &text){
     return result;
 }
 
-station levenshtein_search::find_closest(const std::string &name)
-{
+station levenshtein_search::find_closest(const std::string &name){
     const auto &stations = graph.get_all_stations();
 
     if(stations.empty())
         throw std::runtime_error("no station available");
 
-
     std::string normalized_query = to_lower(name);
 
     station ans;
     int min_dict = std::numeric_limits<int>::max();
-    for( auto st : stations){
-        const std::string& station_name = st.get_name();
-        std::string normalized_station_name = to_lower(station_name);
+
+    bool found_prefix_match = false;
+    size_t shortest_prefix_len = std::numeric_limits<size_t>::max();
+
+    for(auto st : stations){
+        std::string normalized_station_name = to_lower(st.get_name());
+
         // Exact match
-        if(normalized_query == normalized_station_name) return st;
+        if(normalized_query == normalized_station_name)
+            return st;
 
         // Prefix match
-        if(normalized_station_name.rfind(normalized_query, 0) == 0) return st;
+        if(!normalized_query.empty() && normalized_station_name.rfind(normalized_query, 0) == 0){
+            found_prefix_match = true;
 
-        int dict = levenshtein_distance(normalized_query, normalized_station_name);
-        if(min_dict > dict){
-            ans = st;
-            min_dict = dict;
+            if(normalized_station_name.size() < shortest_prefix_len){
+                shortest_prefix_len = normalized_station_name.size();
+                ans = st;
+            }
+            continue;
+        }
+
+        if(!found_prefix_match){
+            int dict = levenshtein_distance(normalized_query, normalized_station_name);
+
+            if(min_dict > dict){
+                ans = st;
+                min_dict = dict;
+            }
         }
     }
-
     return ans;
 }
 
-int levenshtein_search::levenshtein_distance(
-    const std::string &first,
-    const std::string &second
-)
-{
-    std::vector<std::vector<int>> dp(
-        first.size() + 1,
-        std::vector<int>(second.size() + 1)
-    );
+int levenshtein_search::levenshtein_distance(const std::string &first, const std::string &second){
+
+    std::vector<std::vector<int>> dp(first.size() + 1, std::vector<int>(second.size() + 1));
 
     for (int i = 0; i <= static_cast<int>(first.size()); i++)dp[i][0] = i;
-    
 
     for (int j = 0; j <= static_cast<int>(second.size()); j++)dp[0][j] = j;
 
-    
-    
-
     for (int i = 1; i <= static_cast<int>(first.size()); i++){
         for (int j = 1; j <= static_cast<int>(second.size()); j++){
-        
-            if (first[i - 1] == second[j - 1]){
+
+            if (first[i - 1] == second[j - 1])
                 dp[i][j] = dp[i - 1][j - 1];
-            }
             else{
                 dp[i][j] =
+
                     1 + std::min({
                         dp[i - 1][j],
                         dp[i][j - 1],
                         dp[i - 1][j - 1]
+
                     });
             }
         }
     }
-
+    
     return dp[first.size()][second.size()];
 }
